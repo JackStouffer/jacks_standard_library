fe0c2915223cc999dac81d8b4f81dabaa862f24c
Jack Stouffer <jack.stouffer@mcg.com>
2025-12-21 02:29:30 -0500

    working on tooling

--- tools/src/embed.c
+++ tools/src/embed.c
@@ -22,22 +22,31 @@
 #include "../../src/jsl_core.c"
 #include "../../src/jsl_string_builder.c"
 #include "../../src/jsl_os.c"
+#include "../../src/jsl_str_to_str_map.c"
+#include "../../src/jsl_str_to_str_multimap.c"
+#include "../../src/jsl_cmd_line.c"
 
-int32_t main(int32_t argc, char **argv)
+static int32_t entrypoint(JSLCmdLine* cmd, JSLArena* arena)
 {
-    JSLArena arena;
-    jsl_arena_init(&arena, malloc(JSL_MEGABYTES(32)), JSL_MEGABYTES(32));
-
     JSLFatPtr file_path = {0};
     JSLFatPtr file_contents = {0};
     int32_t load_errno = 0;
 
-    if (argc == 2)
-    {
-        file_path = jsl_fatptr_from_cstr(argv[1]);
+    jsl_cmd_line_pop_arg_list(cmd, &file_path);
 
+    if (jsl_cmd_line_pop_arg_list(cmd, &file_path))
+    {
+        jsl_format_file(
+            stderr,
+            JSL_FATPTR_EXPRESSION("Only provide zero or one file path\n")
+        );
+        return EXIT_FAILURE;
+    }
+
+    if (file_path.data != NULL)
+    {
         JSLLoadFileResultEnum load_result = jsl_load_file_contents(
-            &arena,
+            arena,
             file_path,
             &file_contents,
             &load_errno
@@ -51,11 +60,11 @@ int32_t main(int32_t argc, char **argv)
                 file_contents,
                 load_errno
             );
-            exit(EXIT_FAILURE);
+            return EXIT_FAILURE;
         }
 
         JSLStringBuilder builder;
-        jsl_string_builder_init(&builder, &arena);
+        jsl_string_builder_init(&builder, arena);
 
         generate_embed_header(
             &builder,
@@ -77,14 +86,88 @@ int32_t main(int32_t argc, char **argv)
 
         return EXIT_SUCCESS;
     }
-    else if (argc == 1)
-    {
-        jsl_format_file(stderr, JSL_FATPTR_EXPRESSION("Must provide a file"));
-        return EXIT_FAILURE;
-    }
     else
     {
-        jsl_format_file(stderr, JSL_FATPTR_EXPRESSION("Only one file path allowed"));
-        return EXIT_FAILURE;
+        JSLFatPtr stdin_buffer = jsl_arena_allocate(arena, 4096, false);
+        JSLFatPtr stdin_buffer_read_buffer = stdin_buffer;
+
+        size_t n;
+        while ((n = fread(stdin_buffer_read_buffer.data, 1, stdin_buffer_read_buffer.length, stdin)) > 0)
+        {
+            printf("n %lu\n", n);
+            jsl_format_file(stdout, stdin_buffer);
+            JSL_FATPTR_ADVANCE(stdin_buffer_read_buffer, (int64_t) n);
+
+            if (stdin_buffer_read_buffer.length < 32)
+            {
+                stdin_buffer = jsl_arena_reallocate(arena, stdin_buffer, stdin_buffer.length + 4096);
+                stdin_buffer_read_buffer.length += 4096;
+            }
+        }
+
+        JSLFatPtr written_to = jsl_fatptr_auto_slice(stdin_buffer, stdin_buffer_read_buffer);
+        jsl_format_file(stdout, written_to);
     }
+
+    return EXIT_SUCCESS;
 }
+
+#if JSL_IS_WINDOWS
+
+    #include <windows.h>
+    #include <shellapi.h>
+    #pragma comment(lib, "shell32.lib")
+
+    int32_t wmain(int32_t argc, wchar_t** argv);
+
+    int32_t wmain(int32_t argc, wchar_t** argv)
+    {
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+        _setmode(_fileno(stdout), _O_BINARY);
+        _setmode(_fileno(stderr), _O_BINARY);
+
+        int64_t arena_size = JSL_MEGABYTES(32);
+        JSLArena arena;
+        void* backing_data = malloc((size_t) arena_size);
+        if (backing_data == NULL)
+            return EXIT_FAILURE;
+
+        jsl_arena_init(&arena, backing_data, arena_size);
+
+        JSLCmdLine cmd;
+        jsl_cmd_line_init(&cmd, &arena);
+        jsl_cmd_line_parse_wide(&cmd, argc, argv);
+
+        return entrypoint(&cmd, &arena);
+    }
+
+#else
+
+    int32_t main(int32_t argc, char **argv)
+    {
+        int64_t arena_size = JSL_MEGABYTES(32);
+        JSLArena arena;
+        void* backing_data = malloc((size_t) arena_size);
+        if (backing_data == NULL)
+            return EXIT_FAILURE;
+
+        jsl_arena_init(&arena, backing_data, arena_size);
+
+        JSLCmdLine cmd;
+        if (!jsl_cmd_line_init(&cmd, &arena))
+        {
+            jsl_format_file(stderr, JSL_FATPTR_EXPRESSION("Command line input exceeds memory limit"));
+            return EXIT_FAILURE;
+        }
+
+        if (!jsl_cmd_line_parse(&cmd, argc, argv))
+        {
+            jsl_format_file(stderr, JSL_FATPTR_EXPRESSION("Parsing failure"));
+            return EXIT_FAILURE;
+        }
+
+        return entrypoint(&cmd, &arena);
+    }
+
+#endif
